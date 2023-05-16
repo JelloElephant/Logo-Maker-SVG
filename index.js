c2d008e6ae90c3f6cb919b71aa1a3924e8e7563c
Jake Eik <eik995@hotmail.com>
2023-05-15 19:30:10 -0500

    Added functions to Shape and child classes

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const fs = requre('fs')
+const fs = require('fs');
 const inquirer = require('inquirer');
-const shapes = require('./lib/shapes')
+const shapes = require('./lib/shapes.js')
 
 const questions = [
     {
         type: 'input',
         message: 'Enter up to three characters for your logo',
-        name: 'logoChr',
+        name: 'txt',
     },
     {
         type: 'input',
         message: 'Enter a color for your logos text',
-        name: 'logoColor',
+        name: 'txtColor',
     },
     {
         type: 'list',
         message: 'Choose a shape for your logo',
-        name: 'logoShape',
+        name: 'shape',
         choices: ['Circle', 'Triangle', 'Square']
     },
     {
@@ -26,10 +26,23 @@ const questions = [
     },
 ]
 
+function writeFile(filename, data) {
+    fs.appendFile(`${filename}.svg`, data, (err) => err ? console.log(err) : console.log('File created!'))
+}
+
 function init() {
     inquirer.prompt(questions)
     .then(ans => {
-        
+        if(ans.shape == 'Circle') {
+            let media = new shapes.Circle(ans.shapeColor, ans.txt, ans.txtColor)
+            writeFile('logo', media.render())
+        }else if (ans.shape == 'Triangle') {
+            let media = new shapes.Triangle(ans.shapeColor, ans.txt, ans.txtColor)
+            writeFile('logo', media.render())
+        }else if (ans.shape == 'Square') {
+            let media = new shapes.Square(ans.shapeColor, ans.txt, ans.txtColor)
+            writeFile('logo', media.render())
+        }
     })
 }
 
